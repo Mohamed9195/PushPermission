fbf0801c290e4f0dda22eee16879eb078c5d498a
Mohamed Hashem <mohamedhashem@xtrava.co>
2020-09-17 17:00:13 +0200

    Initial Commit

---
+++ PushPermission/PushPermission.h
@@ -0,0 +1,19 @@
+//
+//  PushPermission.h
+//  PushPermission
+//
+//  Created by mohamed hashem on 9/17/20.
+//  Copyright © 2020 mohamed hashem. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PushPermission.
+FOUNDATION_EXPORT double PushPermissionVersionNumber;
+
+//! Project version string for PushPermission.
+FOUNDATION_EXPORT const unsigned char PushPermissionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PushPermission/PublicHeader.h>
+
+
